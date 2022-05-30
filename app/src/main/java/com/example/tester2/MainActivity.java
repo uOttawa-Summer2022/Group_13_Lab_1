8678ba15ce32220fdc65ca1afb4c047682a7cbe5
sshar259 <106174806+sshar259@users.noreply.github.com>
2022-05-29 23:50:48 -0400

    Basic calculator functinality

--- app/src/main/java/com/example/tester2/MainActivity.java
+++ app/src/main/java/com/example/tester2/MainActivity.java
@@ -16,23 +16,26 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.tester2.databinding.ActivityMainBinding;
 
-import java.math.BigDecimal;
-
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener, View.OnClickListener {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
-    Button btn1;
-    Button btn2;
-    Button btn_addition, btn_subtraction, btn_division, btn_multiplication, btn_equal, btn_decimal, btn_clear;
+    Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn_clear,
+            btn0,btn_division,btn_equal,btn_addition,btn_subtraction,btn_decimal,btn_multiplication;
     TextView text_display;
     double num1, num2;
     boolean isNumber, mul, div, isDecimal;
     ScriptEngine engine;
+    String op1 = "";
+    String op2 = "";
+    String operand = "";
+    String result="";
+    boolean isOperandSet = false;
+    boolean resetText= false;
+    Calculator calc = new Calculator();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,13 +57,22 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
       //  });
         btn1 = (Button) findViewById(R.id.btn1);
         btn2 = (Button) findViewById(R.id.btn2);
-        btn_addition = (Button) findViewById(R.id.btn_addition);
-        btn_subtraction= (Button) findViewById(R.id.btn_subtraction);
-        btn_multiplication = (Button) findViewById(R.id.btn_multiplication);
-        btn_division = (Button) findViewById(R.id.btn_division);
-        btn_equal= (Button) findViewById(R.id.btn_equal);
+       /* btn3 = (Button) findViewById(R.id.btn3);
+        btn4 = (Button) findViewById(R.id.btn4);
+        btn5 = (Button) findViewById(R.id.btn5);
+        btn6 = (Button) findViewById(R.id.btn6);
+        btn7 = (Button) findViewById(R.id.btn7);
+        btn8 = (Button) findViewById(R.id.btn8);
+        btn9 = (Button) findViewById(R.id.btn9);*/
         btn_clear = (Button) findViewById(R.id.btn_clear);
+        btn0 = (Button) findViewById(R.id.button0);
+        btn_division = (Button) findViewById(R.id.btn_division);
+        btn_equal = (Button) findViewById(R.id.btn_equal);
+        btn_addition = (Button) findViewById(R.id.btn_addition);
+        btn_subtraction = (Button) findViewById(R.id.btn_subtraction);
+        btn_multiplication = (Button) findViewById(R.id.btn_multiplication);
         btn_decimal = (Button) findViewById(R.id.btn_decimal);
+
         text_display = (TextView) findViewById(R.id.text_display);
         setClickListeners();
         engine = new ScriptEngineManager().getEngineByName("rhino");
@@ -69,6 +81,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     void setClickListeners(){
         btn1.setOnClickListener(this);
         btn2.setOnClickListener(this);
+        btn0.setOnClickListener(this);
         btn_addition.setOnClickListener(this);
         btn_multiplication.setOnClickListener(this);
         btn_subtraction.setOnClickListener(this);
@@ -76,6 +89,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         btn_equal.setOnClickListener(this);
         btn_clear.setOnClickListener(this);
         btn_decimal.setOnClickListener(this);
+        text_display.setOnClickListener(this);
     }
 
     @Override
@@ -119,7 +133,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     public void onClick(View v) {
         // this is the place where I will add code
-        switch (v.getId()) {
+        /*switch (v.getId()) {
             case R.id.btn1:
                 text_display.setText(text_display.getText()+"1"); //
                 break;
@@ -157,11 +171,101 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                     text_display.setText("NO EXPRESSIONS");
                 }
                 break;
+        }*/
+        int temp=v.getId();
+        if(resetText){
+            text_display.setText("");
+            resetText = false;
+        }
+
+        switch(temp) {
+            case R.id.button0:
+                if (isOperandSet == false) {
+                    op1 = op1 + btn0.getText();
+
+                    text_display.setText((text_display.getText().toString().concat(btn0.getText().toString())));
+                } else {
+                    op2 = op2.concat(btn0.getText().toString());
+                    text_display.setText((text_display.getText().toString().concat(btn0.getText().toString())));
+                }
+                break;
+            case R.id.btn1:
+                if (isOperandSet == false) {
+                    op1 = op1 + btn1.getText();
+
+                    text_display.setText((text_display.getText().toString().concat(btn1.getText().toString())));
+                } else {
+                    op2 = op2.concat(btn1.getText().toString());
+                    text_display.setText((text_display.getText().toString().concat(btn1.getText().toString())));
+                }
+
+                break;
+            case R.id.btn2:
+                if (isOperandSet == false) {
+                    op1 = op1.concat(btn2.getText().toString());
+                    text_display.setText((text_display.getText().toString().concat(btn2.getText().toString())));
+
+                }else {
+                    op2 =op2.concat( btn2.getText().toString());
+                    text_display.setText((text_display.getText().toString().concat(btn2.getText().toString())));
+                }
+                break;
+            case R.id.btn_addition:
+                if (isOperandSet == false){
+                    operand = btn_addition.getText().toString();
+                    isOperandSet = true;
+                    text_display.setText(text_display.getText().toString().concat(operand));
+
+                }
+                break;
+            case R.id.btn_subtraction:
+                if (isOperandSet == false){
+                    operand = btn_subtraction.getText().toString();
+                    isOperandSet = true;
+                    text_display.setText(text_display.getText().toString().concat(operand));
+                }
+                break;
+            case R.id.btn_multiplication:
+                if (isOperandSet == false){
+                    operand = btn_multiplication.getText().toString();
+                    isOperandSet = true;
+                    text_display.setText(text_display.getText().toString().concat(operand));
+                }
+                break;
+            case R.id.btn_division:
+                if (isOperandSet == false){
+                    operand = btn_division.getText().toString();
+                    isOperandSet = true;
+                    text_display.setText(text_display.getText().toString().concat(operand));
+                }
+                break;
+            case R.id.btn_equal:
+                if(!op1.equals("") && !op2.equals("") && !operand.equals("")){
+                    calc.setOperands(op1,op2);
+                    calc.setArithmeticOperator(operand);
+                    result = calc.getResult();
+                    text_display.setText(result);
+                    op1 = "";
+                    op2 = "";
+                    operand = "";
+                    isOperandSet = false;
+                    resetText = true;
+                }
+
+                break;
+            case R.id.btn_clear:
+                text_display.setText("");
+                op1 = "";
+                op2 = "";
+                operand = "";
+                isOperandSet = false;
+                resetText = true;
+                break;
         }
     }
-    private String evaluate(String expression) throws ScriptException {
+   /* private String evaluate(String expression) throws ScriptException {
         String result = engine.eval(expression).toString();
         BigDecimal decimal = new BigDecimal(result);
         return decimal.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString();
-    }
+    }*/
 }
